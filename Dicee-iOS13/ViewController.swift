f6390434637e160145a06446f74236e36c5ac4da
Nishu Nishanta <nishu.learner@gmail.com>
2025-02-18 00:51:50 +0530

    Initial commit

---
+++ Dicee-iOS13/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Nishu Nishanta on 11/06/2019.
+//  Copyright © nishuNishanta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+   
+
+    @IBAction func buttonAction(_ sender: Any) {
+        
+        var diceArray = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        
+        
+       
+    }
+    
+}
+
